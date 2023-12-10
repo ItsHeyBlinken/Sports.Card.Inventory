ce2c3fc173e48263b4c2b351a636dd5d56a4c4a5
ItsHeyBlinken <147013156+ItsHeyBlinken@users.noreply.github.com>
2023-12-10 13:24:40 -0600

    update

--- Sports Card App/server.js
+++ Sports Card App/server.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // replace with your MySQL username
-  password: 'Bnasty2215!', // replace with your MySQL password
-  database: 'Card_Inventory',
+  user: 'root',
+  password: 'Bnasty2215!',
+  database: 'sports_card_inventory',
   insecureAuth: true,
 });
 
@@ -26,38 +26,25 @@ db.connect(err => {
   console.log('Connected to MySQL');
 });
 
-// Your API endpoints will be added here
-// Add a card
+// Endpoint to add a card to the database
 app.post('/addCard', (req, res) => {
-    const { set_name, year, card_number, player_name } = req.body;
-    const sql = 'INSERT INTO cards (set_name, year, card_number, player_name) VALUES (?, ?, ?, ?)';
-  
-    db.query(sql, [set_name, year, card_number, player_name], (err, result) => {
-      if (err) {
-        console.error('Error adding card:', err);
-        res.status(500).json({ error: 'Error adding card' });
-        return;
-      }
-      res.status(200).json({ message: 'Card added successfully' });
-    });
-  });
-  
-  // Get all cards with optional search
-  app.get('/getCards', (req, res) => {
-    const searchQuery = req.query.search || '';
-    const sql = 'SELECT * FROM cards WHERE set_name LIKE ? OR player_name LIKE ?';
-  
-    db.query(sql, [`%${searchQuery}%`, `%${searchQuery}%`], (err, results) => {
-      if (err) {
-        console.error('Error retrieving cards:', err);
-        res.status(500).json({ error: 'Error retrieving cards' });
-        return;
-      }
-      res.status(200).json(results);
-    });
-  });
+  const { set_name, year, card_number, player_name } = req.body;
 
-// 
+  // Check if all required fields are provided
+  if (!set_name || !year || !card_number || !player_name) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  // Insert the card into the database
+  const sql = 'INSERT INTO cards (set_name, year, card_number, player_name) VALUES (?, ?, ?, ?)';
+  db.query(sql, [set_name, year, card_number, player_name], (err, result) => {
+    if (err) {
+      console.error('Error adding card:', err);
+      return res.status(500).json({ error: 'Error adding card to the database' });
+    }
+    return res.status(200).json({ message: 'Card added successfully' });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
